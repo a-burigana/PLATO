4e24052312cb37a187b0a0e723c4f34f8062a57d
Alessandro.Burigana <burigana@inf.unibz.it>
2022-02-03 22:36:47 +0100

    Improved delphic script

--- delphic.sh
+++ delphic.sh
@@ -1,7 +1,66 @@
-INSTANCE="$1"    # Name of the instance
-shift            # Shift all arguments to the left (original $1 gets lost)
-ARGUMENTS=("$@") # Rebuild the array with rest of arguments
 
-DOMAIN_PATH=$(dirname "$INSTANCE")
+#!/bin/bash
+# INSTANCE="$1"    # Name of the instance
+# shift            # Shift all arguments to the left (original $1 gets lost)
+# ARGUMENTS=("$@") # Rebuild the array with rest of arguments
+PYTHON_PATH="scripts/python"
+OUT_PATH="out/states"
 
-clingo plato.lp --configuration=frumpy --heuristic=Vsids -t 2 $DOMAIN_PATH/domain.lp $INSTANCE;
+clingo_args=""
+instance=""
+domain_path=""
+semantics="semantics/ma_rho.lp"
+config="run_config/find_plan.lp"
+print=false
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    -i|--instance)
+        instance="$2"
+        domain_path=$(dirname "$instance")
+        shift # past argument
+        shift # past value
+        ;;
+    -s|--semantics)
+        if [ "$2" = poss ] ; then
+            semantics="semantics/ma_rho.lp"
+        elif [ "$2" = kripke ] ; then
+            semantics="semantics/ma_star.lp"
+        else
+            echo "Unknown semantics $input"
+            exit 1
+        fi
+        shift # past argument
+        shift # past value
+        ;;
+    -d|--debug)
+        config="run_config/debug.lp"
+        shift # past argument=value
+        ;;
+    -p|--print)
+        print=true
+        shift # past argument with no value
+        ;;
+    -*|--*)
+        clingo_args+=" ${1}"
+        shift # past argument
+        ;;
+    *)
+        clingo_args+=" ${1}" # save positional arg
+        shift # past argument
+        ;;
+    esac
+done
+
+if [ "$print" = false ] ; then
+    clingo plato.lp $clingo_args $config $semantics --configuration=frumpy --heuristic=Vsids -t 2 $domain_path/domain.lp $instance
+else
+    mkdir -p $OUT_PATH
+
+    clingo plato.lp $clingo_args $config $semantics run_config/print.lp --configuration=frumpy --heuristic=Vsids -t 2 $domain_path/domain.lp $instance > $OUT_PATH/output.txt;
+    python3 $PYTHON_PATH/out_render.py $OUT_PATH/output;
+    dot -Tpdf $OUT_PATH/output.dot > $OUT_PATH/output.pdf;
+
+    rm $OUT_PATH/output.txt;
+    rm $OUT_PATH/output.dot;
+fi
