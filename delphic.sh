d40ae689a1d7d5938d610ecce4b069fb6d331976
Alessandro.Burigana <burigana@inf.unibz.it>
2022-02-03 12:45:06 +0100

    Tweaking scripts for easier usage

--- delphic.sh
+++ delphic.sh
@@ -1,7 +1,7 @@
-DOMAIN="$1"      # Name of the domain
-INSTANCE="$2"    # Name of the instance
+INSTANCE="$1"    # Name of the instance
 shift            # Shift all arguments to the left (original $1 gets lost)
-shift            # Shift all arguments to the left (original $2 gets lost)
 ARGUMENTS=("$@") # Rebuild the array with rest of arguments
 
-clingo plato.lp --configuration=frumpy --heuristic=Vsids -t 2 $DOMAIN/domain.lp $DOMAIN/$INSTANCE;
+DOMAIN_PATH=$(dirname "$INSTANCE")
+
+clingo plato.lp --configuration=frumpy --heuristic=Vsids -t 2 $DOMAIN_PATH/domain.lp $INSTANCE;
--- scripts/asp_print.sh
+++ scripts/asp_print.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-DOMAIN="$1"      # Name of the domain
-INSTANCE="$2"    # Name of the instance
+INSTANCE="$1"    # Name of the instance
 shift            # Shift all arguments to the left (original $1 gets lost)
-shift            # Shift all arguments to the left (original $2 gets lost)
 ARGUMENTS=("$@") # Rebuild the array with rest of arguments
 PLATO_PATH=".."
 PYTHON_PATH="scripts/python"
 OUT_PATH="out/states"
-# DOMAIN_PATH="../exp/ICLP20"
+DOMAIN_PATH=$(dirname "$INSTANCE")
 
 mkdir -p $OUT_PATH
 
 cd $PLATO_PATH
-clingo plato.lp --configuration=frumpy --heuristic=Vsids -t 2 $DOMAIN/domain.lp $DOMAIN/$INSTANCE > $OUT_PATH/output.txt;
+clingo plato.lp --configuration=frumpy --heuristic=Vsids -t 2 $DOMAIN_PATH/domain.lp $INSTANCE > $OUT_PATH/output.txt;
 python3 $PYTHON_PATH/out_render.py $OUT_PATH/output;
 dot -Tpdf $OUT_PATH/output.dot > $OUT_PATH/output.pdf;
 
